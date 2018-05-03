989dd8e50c79ddb31f4a918fac3b772b509d6db2
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2018-05-03 10:58:56 +0200

    Added sigmoid loss

--- model/losses.py
+++ model/losses.py
@@ -1,3 +1,6 @@
 import tensorflow as tf
 
 softmax_cross_entropy_with_logits = tf.nn.softmax_cross_entropy_with_logits
+sigmoid_cross_entropy_with_logits = tf.nn.sigmoid_cross_entropy_with_logits
+
+
