1aea976ee942d12910a51f3fab817b089427b788
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2018-05-04 15:44:27 +0200

    Added classification evaluator classes

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,7 @@
+__author__ = 'Yngve Mardal Moe'
+__email__ = 'yngve.m.moe@gmail.com'
+
+
+from .evaluator import ClassificationEvaluator, BinaryClassificationEvaluator
 from .logger import TensorboardLogger
 from .dataloader import HDFReader, HDFData
