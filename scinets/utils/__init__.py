e69701cf1011614a2d75bb6e3e13af87853f4b26
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2018-08-06 09:05:33 +0200

    Added SacredExperiment to the utils __init__

--- scinets/utils/__init__.py
+++ scinets/utils/__init__.py
@@ -3,5 +3,6 @@ __email__ = 'yngve.m.moe@gmail.com'
 
 
 from .evaluator import ClassificationEvaluator, BinaryClassificationEvaluator
-from .logger import TensorboardLogger
+from .logger import TensorboardLogger, SacredLogger
 from .dataloader import HDFReader, HDFData
+from .experiment import NetworkExperiment, SacredExperiment
