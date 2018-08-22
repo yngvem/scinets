49ef5280feaff97bee22a0200978389a6c5554a8
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2018-08-22 13:45:24 +0200

    Updated experiment test file

--- experiment_test.py
+++ experiment_test.py
@@ -1,41 +1,109 @@
 import tensorflow as tf
-from scinets.model import NeuralNet
-from scinets.utils import TensorboardLogger, HDFReader, BinaryClassificationEvaluator
-from scinets.utils.experiment import NetworkExperiment
-from scinets.trainer import NetworkTrainer
-from tqdm import trange
-import sys
+from scinets.utils.experiment import NetworkExperiment, MNISTExperiment
 
 
 
 if __name__ == '__main__':
-    experiment_params={
+    experiment_params = {
         'log_dir': './logs/',
         'name': 'test_experiment',
         'continue_old': False,
         'num_steps': 1000
     }
-
     dataset_params = {
         'data_path': '/home/yngve/dataset_extraction/val_split_2d.h5',
         'batch_size': [64, 64, 1],
         'val_group': 'val'
     }
-    model_params={
+    architecture = [
+        {
+            'layer': 'ResnetConv2D',
+            'scope': 'resnet1',
+            'layer_params': {
+                'out_size': 8,
+                'k_size': 3,
+            },
+            'normalization': {
+                'operator': 'batch_normalization'
+            },
+            'activation': {
+                'operator': 'relu'
+            }
+        },
+        {
+            'layer': 'ResnetConv2D',
+            'scope': 'resnet2',
+            'layer_params': {
+                'out_size': 16,
+                'k_size': 3,
+                'strides': 2
+            },
+            'normalization': {
+                'operator': 'batch_normalization'
+            },
+            'activation': {
+                'operator': 'relu'
+            }
+        },
+        {
+            'layer': 'ResnetConv2D',
+            'scope': 'resnet3',
+            'layer_params': {
+                'out_size': 16,
+                'k_size': 3,
+            },
+            'normalization': {
+                'operator': 'batch_normalization'
+            },
+            'activation': {
+                'operator': 'relu'
+            }
+        },
+        {
+            'layer': 'LinearInterpolate',
+            'scope': 'upsample',
+            'layer_params': {'rate': 2}
+        },
+        {
+            'layer': 'ResnetConv2D',
+            'scope': 'resnet4',
+            'layer_params': {
+                'out_size': 32,
+                'k_size': 3,
+                'strides': 2
+            },
+            'normalization': {
+                'operator': 'batch_normalization'
+            },
+            'activation': {
+                'operator': 'relu'
+            }
+        },
+        {
+            'layer': 'ResnetConv2D',
+            'scope': 'resnet5',
+            'layer_params': {
+                'out_size': 10,
+                'k_size': 3,
+            },
+            'normalization': {
+                'operator': 'batch_normalization'
+            },
+            'activation': {
+                'operator': 'linear'
+            }
+        },
+        {
+            'layer': 'GlobalAveragePool',
+            'scope': 'global_average'
+        }
+    ]
+    model_params = {
         'type': 'NeuralNet',
         'network_params': {
             'loss_function': 'sigmoid_cross_entropy_with_logits',
             'loss_kwargs': {},
-            'architecture': [
-                {
-                    'layer': 'conv2d',
-                    'scope': 'conv1',
-                    'out_size': 1,
-                    'k_size': 5,
-                    'batch_norm': True,
-                    'activation': 'linear',
-                }
-            ],
+            'architecture': architecture,
             'verbose': True,
         }
     }
@@ -45,64 +113,35 @@ if __name__ == '__main__':
     log_params={
         'val_log_frequency': 10,
         'evaluator': 'BinaryClassificationEvaluator',
-        'sacred_params': {
-            'log_dict': {
-                'loss': 'Loss'
-            }
-        }
         'tb_params': {
-            'log_dict': {
-                'loss': [
+            'log_dicts': [
                     {
                         'log_name': 'Loss',
+                        'log_var': 'loss',
                         'log_type': 'scalar'
-                    }
-                ],
-                'probabilities': [
-                    {
-                        'log_name': 'Probability_map',
-                        'log_type': 'image',
-                        'log_kwargs': {'max_outputs':1}
-                    }
-                ],
-                'accuracy': [
+                    },
                     {
                         'log_name': 'Accuracy',
+                        'log_var': 'accuracy',
                         'log_type': 'scalar'
-                    }
-                ],
-                'dice': [
+                    },
                     {
                         'log_name': 'Dice',
+                        'log_var': 'dice',
                         'log_type': 'scalar'
-                    }
-                ],
-                'true_out': [
+                    },
                     {
-                        'log_name': 'Mask',
-                        'log_type': 'image',
-                        'log_kwargs': {'max_outputs':1}
-                    }
-                ],
-                'input': [
-                    {
-                        'log_name': 'CT',
+                        'log_name': 'Images',
+                        'log_var': 'input',
                         'log_type': 'image',
                         'log_kwargs': {'max_outputs': 1,
                                 'channel': 0}
                     },
-                    {
-                        'log_name': 'PET',
-                        'log_type': 'image',
-                        'log_kwargs': {'max_outputs': 1,
-                                'channel': 1}
-                    }
-                ]
-            }
+            ]
         }
     }
 
-    experiment = NetworkExperiment(
+    experiment = MNISTExperiment(
         experiment_params=experiment_params,
         model_params=model_params,
         dataset_params=dataset_params,
@@ -110,4 +149,3 @@ if __name__ == '__main__':
         log_params=log_params
     )
 
-    experiment.train(1000, 10)
