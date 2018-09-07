d14c77f47d27be5c63343bf5e9bba34a83f8f7f8
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2018-09-07 12:42:38 +0200

    Added dice loss (untested)

--- scinets/model/losses.py
+++ scinets/model/losses.py
@@ -4,3 +4,10 @@ softmax_cross_entropy_with_logits = tf.nn.softmax_cross_entropy_with_logits
 sigmoid_cross_entropy_with_logits = tf.nn.sigmoid_cross_entropy_with_logits
 
 
+def binary_dice(prediction, target, scope='binary_dice_loss'):
+    with tf.variable_scope(scope):
+        dice_numerator = 2*tf.reduce_sum(prediction*target)
+        dice_denominator = (tf.reduce_sum(tf.square(target)) + 
+                            tf.reduce_sum(tf.square(prediction))
+        return dice_numerator/dice_denominator
+    
