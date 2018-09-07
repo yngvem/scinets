db60b4d60ab35a3fa8807ea1fcfb62f9e3b6890a
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2018-09-07 16:33:48 +0200

    Increased numerical stability with (a+eps)/(b+eps)

--- scinets/model/losses.py
+++ scinets/model/losses.py
@@ -12,11 +12,14 @@ def sigmoid_cross_entropy_with_logits(prediction, target, name):
                                              name=name)
 
 def binary_dice(prediction, target, name='binary_dice_loss'):
+    size = len(prediction.get_shape().as_list())
+    reduce_ax = list(range(1, size))
+    eps = 1e-4
+
     with tf.variable_scope(name):
-        size = len(prediction.get_shape().as_list())
-        reduce_ax = list(range(1, size))
-        dice_numerator = 2*tf.reduce_sum(prediction*target, axis=reduce_ax)
+        dice_numerator = 2*tf.reduce_sum(prediction*target, axis=reduce_ax) + eps
         dice_denominator = (tf.reduce_sum(tf.square(target), axis=reduce_ax) + 
-                            tf.reduce_sum(tf.square(prediction), axis=reduce_ax))
-        return dice_numerator/dice_denominator
+                            tf.reduce_sum(tf.square(prediction), axis=reduce_ax)+
+                            eps)
+        return 1-dice_numerator/dice_denominator
     
