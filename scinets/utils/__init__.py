2340b6bde481c8e8639c1a7f05784afed1b8346d
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2018-10-17 13:38:45 +0200

    Update __init__

--- scinets/utils/__init__.py
+++ scinets/utils/__init__.py
@@ -5,3 +5,4 @@ __email__ = 'yngve.m.moe@gmail.com'
 from .evaluator import ClassificationEvaluator, BinaryClassificationEvaluator
 from .logger import TensorboardLogger, SacredLogger
 from .experiment import NetworkExperiment, SacredExperiment, MNISTExperiment
+from .experiment_tester import ExperimentTester
