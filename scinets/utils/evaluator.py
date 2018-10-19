dda6dcfcfc2d9d3dafc3a9ce5cf4b978f7737e4b
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2018-10-19 15:43:52 +0200

    Fixed so h5 datasets are reshaped before adding new elements.

--- scinets/utils/evaluator.py
+++ scinets/utils/evaluator.py
@@ -255,6 +255,11 @@ class NetworkTester:
                         run_ops, feed_dict=feed_dict
                 )
                  
+                idxes.resize((i+1*batch_size,))
+                images.resize((i+1*batch_size, *dataset.data_shape[1:]))
+                prediction.resize((i+1*batch_size, *dataset.target_shape[1:]))
+                masks.resize((i+1*batch_size, *dataset.target_shape[1:]))
+
                 idxes[i*batch_size:(i+1)*batch_size] = curr_idxes
                 images[i*batch_size:(i+1)*batch_size] = curr_images
                 prediction[i*batch_size:(i+1)*batch_size] = curr_prediction
